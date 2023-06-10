09f7c19035ba28bfed18de413559b2d7cec52ff2
hamjaehyeong <ham6729@gmail.com>
2023-06-11 00:54:18 +0900

    틀 완성

--- script.js
+++ script.js
@@ -1,78 +1,159 @@
-const square1 = document.querySelector('.square-1')
-const square2 = document.querySelector('.square-2')
-const square3 = document.querySelector('.square-3')
-const square4 = document.querySelector('.square-4')
-const square5 = document.querySelector('.square-5')
-const square6 = document.querySelector('.square-6')
-const square7 = document.querySelector('.square-7')
-const square8 = document.querySelector('.square-8')
-const square9 = document.querySelector('.square-9')
+const squaresEl = document.querySelectorAll('.square');
+const startButtonEl = document.querySelector('.startButton');
+const countNumberEl = document.querySelector('.countNumber');
+const messageEl = document.querySelector('.message');
+const squaresdivEl = document.querySelector('.squares') //진동효과를 위해 영역도 가져옴
 
-const startButton = document.querySelector('.startButton')
+let squareNum = []; //순서가 담길것
+let counter = 0;    //변화가 적용된 square를 카운트
+let clickCount = 0; //선택된 square를 카운트
 
-const squares = [1,2,3,4,5,6,7,8,9]
-let squareNum = []
+let count = 10;  //제한시간 카운트 조정가능
 
+//1부터 max만큼 중복x 숫자 생성, 순서 배열에 추가
 function generateRandomNum(max) {
-    if(squareNum.length === max) {
+    if (squareNum.length === max) {
         return;
     }
-    let randomNum = Math.floor(Math.random()*max)+1;
-    if(!squareNum.includes(randomNum)){
-        console.log(randomNum)
-        squareNum.push(randomNum)
+    let randomNum = Math.floor(Math.random() * max) + 1;
+    if (!squareNum.includes(randomNum)) {
+        console.log(randomNum);
+        squareNum.push(randomNum);
         return randomNum;
     }
-    else{
+    else {
         return generateRandomNum(max);
     }
 }
 
-function blueSquare(){
+//순서에 따라 square에 효과 주기
+function blueSquare() {
     let generatedNum = generateRandomNum(9);
-    if(generatedNum === undefined) {
-        return; 
+    if (generatedNum === undefined) {
+        return;
     }
-    let blueSquare = document.querySelectorAll(".square")[generatedNum - 1]; 
+    let blueSquare = document.querySelectorAll(".square")[generatedNum - 1];
     blueSquare.classList.add('blue');
 
-    setTimeout(function() {
+    setTimeout(function () {
         blueSquare.classList.remove('blue');
-    }, 1000);
+    }, 500);
 }
 
-let counter = 0;
+//각 square에 이벤트 추가
+function checkClickOrder() {
+    squaresEl.forEach((square) => {
+        square.addEventListener('click', selectSquares);
+    });
+}
 
-startButton.addEventListener('click',function(){
+//박스 진동
+const vibration = (target) => {
+    target.classList.add("vibration");
+
+    setTimeout(function () {
+        target.classList.remove("vibration");
+    }, 400);
+}
+
+//순서 맞추기, 이벤트 제거까지
+function selectSquares(event) {
+    let square = event.target;
+    if (squareNum[clickCount] == square.dataset.number) {
+        square.classList.add('blue');
+        clickCount++;
+        if (clickCount === 9) {
+            messageEl.textContent = '오 다맞았넹~';
+            clearInterval(countInterval);
+            startButtonEl.textContent = '다시 시작';
+            startButtonEl.disabled = false;
+            squaresEl.forEach((square) => {
+                square.removeEventListener('click', selectSquares);
+            });
+            squaresdivEl.classList.remove('cursor')
+        }
+    } else {
+        square.classList.add('red');
+        vibration(squaresdivEl);
+        messageEl.textContent = '틀렸당ㅋ';
+        clearInterval(countInterval);
+        startButtonEl.textContent = '다시 시작';
+        startButtonEl.disabled = false;
+        squaresEl.forEach((square) => {
+            square.removeEventListener('click', selectSquares);
+        });
+        squaresdivEl.classList.remove('cursor')
+    }
+}
+
+//게임 리셋
+function resetGame() {
+    squaresEl.forEach((square) => {
+        square.classList.remove('blue');
+        square.classList.remove('red');
+        square.removeEventListener('click', selectSquares);
+    });
+    messageEl.textContent = '순서 맞춰보세요';
+    countNumberEl.textContent = '10';
     squareNum = [];
-    let count = 10;
+    clickCount = 0;
+    counter = 0;
+    count = 10;
+}
 
-    const countNumber = document.querySelector('.countNumber');
-    countNumber.textContent = count;
+let countInterval;
 
-    const interVal = setInterval(function(){
+//순서 맞출때 카운트
+function countDown() {
+    countInterval = setInterval(function () {
+        count--;
+        countNumberEl.textContent = count;
+        if (count === 0) {
+            clearInterval(countInterval);
+            messageEl.textContent = '시간 초과!';
+            startButtonEl.textContent = '다시 시작';
+            startButtonEl.disabled = false;
+            squaresEl.forEach((square) => {
+                square.removeEventListener('click', selectSquares);
+            });
+            squaresdivEl.classList.remove('cursor')
+
+        }
+    }, 1000);
+};
+
+//스타트 버튼, 게임리셋
+startButtonEl.addEventListener('click', function () {
+    startButtonEl.disabled = true;
+
+    resetGame();
+
+    const interVal = setInterval(function () {
         blueSquare();
         counter++;
-        if(counter === 9){
-            console.log(squareNum)
+        if (counter === 9) {
+            console.log(squareNum);
             counter = 0;
-            clearInterval(interVal)
-            delay(1000,countDown);
+            clearInterval(interVal);
+
+            countNumberEl.textContent = count;
+
+            delay(500, function () {
+                count = 10;
+                countDown()
+
+                checkClickOrder();
+                squaresdivEl.classList.add('cursor')
+            });
         }
-    },1000);
+    }, 500);
 
     function delay(time, callback) {
-        setTimeout(callback, time); 
-      }
+        setTimeout(callback, time);
+    }
+});
 
-function countDown() {
-    const countInterval = setInterval(function() {
-      count--;
-      countNumber.textContent = count;
-      if (count === 0) {
-        clearInterval(countInterval);
-      }
-    }, 1000);
-  };
 
-})
+////////////next step
+//단계 추가
+//승패 효과 추과
